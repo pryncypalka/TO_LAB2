2e99dd1a35eb4bbe8d6b2c2d3f1385495afea04e
Jacek Kamiński <jacus502@gmail.com>
2023-11-14 17:34:44 +0100

    main in progress

--- main.py
+++ main.py
@@ -12,11 +12,27 @@ def main():
     vector2 = Vector3DDecorator(2, 2, 0)
     vector3 = Polar2DInheritance(1, 1)
 
+    print("układ karteziański:")
 
-    # Wyświetlanie wektorów
     print("Vector1: ", vector1.getComponents())
     print("Vector2: ", vector2.getComponents())
     print("Vector3: ", vector3.getComponents())
+
+    print("\n")
+    print("Układ biegunowy: ")
+    print("kąt: ")
+    print("Vector3: ", vector3.getAngle())
+    print("Długość: ")
+    print("Vector3: ", vector3.abs())
+    print("\n")
+
+    print("Iloczyn skalarny: ")
+    print("Vector2 i Vector3: ", vector2.cdot(vector3))
+    print("Vector2 i Vector2: ", vector2.cdot(vector2))
+    print("Vector3 i Vector3: ", vector3.cdot(vector3))
+    print("\n")
+    print("Iloczyn wektorowy: ")
+    print("Vector1 i Vector2: ", vector1.cross(vector2).getComponents())
 
 
 
