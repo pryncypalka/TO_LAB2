b2d1b1ed777b23b043dacc78692b2402ac891263
Jacek Kamiński <jacus502@gmail.com>
2023-11-19 16:03:03 +0100

    task done

--- Vector3DInheritance.py
+++ Vector3DInheritance.py
@@ -11,25 +11,18 @@ class Vector3DInheritance(Vector2D):
         return sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
 
     def cdot(self, param):
-        if isinstance(param, Vector3DInheritance):
-            p_x, p_y, p_z = param.getComponents()
-            return self._x * p_x + self._y * p_y + self._z * p_z
-        else:
-            raise ValueError("Parameter must be an instance of IVector")
+        return super().getComponents()[0] * param.getComponents()[0] + super().getComponents()[1] * param.getComponents()[1] + self._z * 0
+
 
     def getComponents(self):
         return [self._x, self._y, self._z]
 
     def cross(self, param):
-        x1, y1, z1 = self._x, self._y, self._z
-        x2, y2, z2 = param.getComponents()
-
-        # Obliczanie iloczynu wektorowego
-        result_x = y1 * z2 - y2 * z1
-        result_y = z1 * x2 - z2 * x1
-        result_z = x1 * y2 - x2 * y1
-
-        return Vector3DInheritance(result_x, result_y, result_z)
+        _temp_x = super().getComponents()[1] * 0 - self._z * param.getComponents()[1]
+        _temp_y = super().getComponents()[0] * 0 - self._z * param.getComponents()[0]
+        _temp_z = super().getComponents()[0] * param.getComponents()[1] - super().getComponents()[1] * \
+                  param.getComponents()[0]
+        return Vector3DInheritance(_temp_x, _temp_y, _temp_z)
 
     def getSrcV(self):
         return self
--- main.py
+++ main.py
@@ -12,27 +12,29 @@ def main():
     vector2 = Vector3DDecorator(2, 2, 0)
     vector3 = Polar2DInheritance(1, 1)
 
-    print("układ karteziański:")
+    print("Układ karteziański:")
+    print("wektor 3Dinheritance: ", vector1.getComponents())
+    print("wektor 3Ddecorator: ", vector2.getComponents())
+    print("wektor Polar2Dinheritance: ", vector3.getComponents())
 
-    print("Vector1: ", vector1.getComponents())
-    print("Vector2: ", vector2.getComponents())
-    print("Vector3: ", vector3.getComponents())
 
     print("\n")
     print("Układ biegunowy: ")
     print("kąt: ")
-    print("Vector3: ", vector3.getAngle())
-    print("Długość: ")
-    print("Vector3: ", vector3.abs())
+    print("Vector Polar2Dinheritance: ", vector3.getAngle())
+    print("promień wodzący: ")
+    print("Vector Polar2Dinheritance: ", vector3.abs())
     print("\n")
 
     print("Iloczyn skalarny: ")
-    print("Vector2 i Vector3: ", vector2.cdot(vector3))
-    print("Vector2 i Vector2: ", vector2.cdot(vector2))
-    print("Vector3 i Vector3: ", vector3.cdot(vector3))
+    print("Vector 3Ddecorator i Vector Polar2DInheritance: ", vector2.cdot(vector3))
+    print("Vector Polar2DInheritance i Vector Polar2DInheritance: ", vector3.cdot(vector3))
+    print("Vector 3DInheritance i Vector Polar2DInheritance: ", vector1.cdot(vector3))
+
     print("\n")
     print("Iloczyn wektorowy: ")
-    print("Vector1 i Vector2: ", vector1.cross(vector2).getComponents())
+    print("Vector 3DInheritance i Vector 3DDecorator: ", vector1.cross(vector2).getComponents())
+    print("Vector 3DInheritance i Vector Polar2DInheritance: ", vector1.cross(vector3).getComponents())
 
 
 
